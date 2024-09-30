ae380ff374a5b393b4a275a7ef2777940703fd40
Ayushi-soni232 <146701317+Ayushi-soni232@users.noreply.github.com>
2024-09-30 09:24:49 +0000

    Your commit message

--- program1.js
+++ program1.js
@@ -1,11 +1,28 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+// program1.js
+function isValid(s) {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (map[char]) {
+            // If character is an opening bracket, push to stack
+            stack.push(char);
+        } else {
+            // If character is a closing bracket, check if it matches the top of the stack
+            const topElement = stack.pop();
+            if (map[topElement] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -2,9 +2,39 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
+//var romanToInt = function(s) {
     
-};
+//};
 
 
-module.exports={romanToInt}
+//module.exports={romanToInt}
+// program2.js
+function romanToInt(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+
+        // If the next Roman numeral is larger, subtract the current value from total
+        if (next > current) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
+}
+
+module.exports = { romanToInt };
